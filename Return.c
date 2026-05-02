a3794383e218246c96cd60425ca469c76b113e70
Saiiprasad24 <saiprasadshirsath24@gmail.com>
2026-05-02 11:45:00 +0530

    Add Return.c with Display function and main

---
+++ Return.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int Display()
+{
+    return 11;
+}
+
+int main()
+{
+    int Ret = 0;
+
+    Ret = Display();
+
+    printf("%d\n",Ret);
+    
+    return 0;
+}#include<stdio.h>
+
+int Display()
+{
+    return 11;
+}
+
+int main()
+{
+    int Ret = 0;
+
+    Ret = Display();
+
+    printf("%d\n",Ret);
+    
+    return 0;
+}
